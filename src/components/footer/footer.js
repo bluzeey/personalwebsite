477d4f475139c20d5d72dc80564595699d0813b1
Sahil Maheshwari <sahilm1711@gmail.com>
2021-11-03 04:05:05 +0530

    Creating the application from scratch

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -10,12 +10,12 @@ const Footer = () => {
       <LinkList>
         <LinkColumn>
           <LinkTitle>Call</LinkTitle>
-          <LinkItem href="tel:314-343-3432">314-343-3432</LinkItem>
+          <LinkItem href="tel:+918426811477">+918426811477</LinkItem>
         </LinkColumn>
         <LinkColumn>
           <LinkTitle>Email</LinkTitle>
-          <LinkItem href="mailto:contact@jsmastery.com">
-            contact@jsmastery.com
+          <LinkItem href="mailto:sahilm1711@gmail.com">
+            sahilm1711@gmail.com
           </LinkItem>
         </LinkColumn>
       </LinkList>
@@ -24,7 +24,7 @@ const Footer = () => {
           <Slogan>Innovating one project at a time</Slogan>
         </CompanyContainer>
         <SocialContainer>
-          <SocialIcons href="https://google.com">
+          <SocialIcons href="https://github.com/bluzeey">
             <AiFillGithub size="3rem" />
           </SocialIcons>
           <SocialIcons href="https://google.com">
