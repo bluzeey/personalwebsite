191b828d4fe497ed80fb4d6bd965dd20b632faad
Sahil Maheshwari <sahilm1711@gmail.com>
2021-11-04 09:09:52 +0530

    Updating gitignore

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -10,7 +10,7 @@ const Header = () =>  (
     <Div1>
       <Link href="/">
         <a style={{ display: 'flex', alignItems: 'center', color:"white" }}>
-          <DiCssdeck size="3rem" /> <span>DevSahil</span>
+          <DiCssdeck size="3rem"/><span>DevSahil</span>
         </a>
       </Link>
     </Div1>
