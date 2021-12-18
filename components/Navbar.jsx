7c50823615e6f94bfeb70988d1acdfbd7366c9d5
Sahil Maheshwari <sahilm1711@gmail.com>
2021-12-18 17:28:02 +0530

    Adding the background image

---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+    return (
+        <div className="absolute flex items-center justify-around">
+            <p>Home</p>
+            <p>About</p>
+            <p>Resume</p>
+            <p>Projects</p>
+            <p>Contact</p>
+        </div>
+    )
+}
+
+export default Navbar
