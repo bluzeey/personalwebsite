dc04abb494cd42046e586eb0945c812f8be43849
Sahil Maheshwari <sahilm1711@gmail.com>
2021-12-18 19:48:47 +0530

    Adding color to the links

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,8 @@
 import Link from "next/link"
 function Navbar() {
     return (
-        <div className="flex items-center justify-around text-white text-lg h-16 font-semibold">
-            <Link href='/' className="hover:blue-400 cursor-pointer">HOME</Link>
+        <div className="flex items-center justify-around text-white text-lg p-4 font-semibold">
+            <Link href='/' className="hover:bg-violet-400 bg-red-400 cursor-pointer">HOME</Link>
             <a>ABOUT</a>
             <a>RESUME</a>
             <a>PROJECTS</a>
