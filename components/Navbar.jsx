b452a0dae34cc09d8e0dc5c43b618c81755cac51
Sahil Maheshwari <sahilm1711@gmail.com>
2021-12-19 10:10:46 +0530

    Added the classes to the link

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,11 +2,11 @@ import Link from "next/link"
 function Navbar() {
     return (
         <div className="flex items-center justify-around text-white text-lg p-4 font-semibold">
-            <Link href='/' className="hover:bg-black cursor-pointer">HOME</Link>
-            <Link href="/about">ABOUT</Link>
-            <a>RESUME</a>
-            <a>PROJECTS</a>
-            <a>CONTACT</a>
+            <a href='/' className="link">HOME</a>
+            <a href="/about" className="link">ABOUT</a>
+            <a className="link">RESUME</a>
+            <a href="/projects" className="link">PROJECTS</a>
+            <a className="link">CONTACT</a>
         </div>
     )
 }
---
+++ pages/projects.js
@@ -0,0 +1,9 @@
+function projects() {
+    return (
+        <div>
+            These are the projects!
+        </div>
+    )
+}
+
+export default projects
