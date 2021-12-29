595d077a77a770bf7ad0461539d4c84c8d785d87
Sahil Maheshwari <sahilm1711@gmail.com>
2021-12-29 13:52:49 +0530

    Added the thanks mail

--- components/Main.jsx
+++ components/Main.jsx
@@ -30,10 +30,10 @@ function Main() {
           <Animation/>
         </div>
 
-        {/* <div className="file:p-4 second-gradient-color font-inter">
-            <h1 className=" text-center text-5xl font-bold text-transparent bg-clip-text  bg-gradient-to-r from-yellow-700 via-yellow-400 to-yellow-200 m-2 p-4">Some of my projects</h1>
+         <div className="file:p-4 second-gradient-color font-inter">
+            <h1 className=" text-center text-5xl font-bold text-transparent bg-clip-text  bg-gradient-to-r from-yellow-700 via-yellow-400 to-yellow-200 m-2 p-4">{currentLanguage.projectHeading}</h1>
                  <div className="p-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-7">
-                  {currentLanguage.Myprojects.slice(0,3).map(project=>(
+                  {currentLanguage.Myprojects?.slice(0,3).map(project=>(
                     <ProjectCard key={project.id} title={project.title} description={project.description} image={project.image} tags={project.tags}/>
                   ))}
                  </div>
@@ -41,7 +41,7 @@ function Main() {
                <button onClick={()=>{router.push('/projects')}} 
                className="text-white font-bold bg-gradient-to-r from-yellow-500 via-yellow-600 to-yellow-700 hover:bg-gradient-to-br focus:ring-4 focus:ring-yellow-300 dark:focus:ring-yellow-800 rounded-lg text-xl px-5 py-2.5 text-center mr-2 mb-2">See all the projects</button>     
             </div>
-        </div> */}
+        </div> 
           <Contact/>
         </>
     )
