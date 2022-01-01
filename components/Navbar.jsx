53935fce585a05355b2c5dd6895c425d50dc7e37
Sahil Maheshwari <sahilm1711@gmail.com>
2022-01-01 18:37:54 +0530

    correct the download functionality

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -20,7 +20,7 @@ function Navbar() {
         <div className="flex flex-col md:flex-row items-center 
         justify-around bg-clip-text text-transparent second-gradient-color text-lg p-4 font-semibold">
             <a href='/' className="link">{currentLanguage?.Navbar?.[0]}</a>
-            <a href="assets/Resume.pdf" download="w3logo" className="link">{currentLanguage?.Navbar?.[1]}</a>
+            <a href="/Resume.pdf" download="Resume.pdf" className="link">{currentLanguage?.Navbar?.[1]}</a>
             <a href="/projects/" className="link">{currentLanguage?.Navbar?.[2]}</a>
             <a href={`${process.env.URL}#contact`} className="link">{currentLanguage?.Navbar?.[3]}</a>
             <div className="flex w-15 items-center">
