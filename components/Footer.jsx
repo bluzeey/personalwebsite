a6927c3e5e6f7e667818d7982f97063f0ea21f89
Sahil Maheshwari <sahilm1711@gmail.com>
2022-01-07 11:57:30 +0530

    Added Mail Icon

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,17 @@
-import {LinkedIn, FacebookOutlined, GitHub} from '@mui/icons-material'
+import {LinkedIn,GitHub, Markunread} from '@mui/icons-material'
 function Footer() {
     return (
         <footer class="bg-gray-800 w-full py-8">
             <div class="max-w-screen-xl mx-auto px-4">
                 <div class="pt-8 flex max-w-xs mx-auto items-center justify-between">
-                    <a href="https://github.com/bluzeey" target="_blank" class=" hover:text-white transition-colors duration-200">
+                    <a href="https://github.com/bluzeey" target="_blank" className=" hover:text-white transition-colors duration-200">
                         <GitHub className="h-7 text-white"/>
                     </a>
-                    <a href="https://www.linkedin.com/in/sahil-maheshwari-702590228/" target="_blank" class=" hover:text-white transition-colors duration-200">
+                    <a href="https://www.linkedin.com/in/sahil-maheshwari-702590228/" target="_blank" className=" hover:text-white transition-colors duration-200">
                         <LinkedIn className="h-7 text-white"/>
+                    </a>
+                    <a href="mailto:sahilm1711@gmail.com" target="_blank" className="hover:text-white transition-colors duration-200">
+                        <Markunread className="h-7 text-white"/>
                     </a>
 
                 </div>
