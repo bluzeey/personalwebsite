10165bd01380c9c43281e9f525575e191bed961d
Sahil Maheshwari <sahilm1711@gmail.com>
2022-01-16 21:21:18 +0530

    Update Intro.jsx

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -19,7 +19,7 @@ function Intro() {
     return (
         <>
         <div className="flex xs:flex-col lg:flex-row items-center p-5">
-          <img src="codingDev.svg" className="xs:w-full h-2/5 w-2/5 m-5"/>
+          <img src="CodingDev.svg" className="xs:w-full h-2/5 w-2/5 m-5"/>
            <div className="font-inter flex justify-center flex-col font-bold b-1 leading-tight m-5">
             <h1 className="text-5xl text-transparent bg-clip-text second-gradient-color">{currentLanguage.greetings}</h1>  
             <p className="text-transparent bg-clip-text second-gradient-color text-2xl">{currentLanguage.intro?.[0]}</p>
